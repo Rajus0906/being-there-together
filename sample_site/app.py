59edaca72c7400b224b941a27e385901384296e1
Raju sivanantham <90371622+Rajus0906@users.noreply.github.com>
2022-03-28 12:35:40 -0400

    Add files via upload

--- sample_site/app.py
+++ sample_site/app.py
@@ -17,9 +17,22 @@ def index():
     f = open("count.txt", "w")
     f.write(str(count))
     f.close()
+    
+    if count % 1000 == 0:
+        return render_template("index.html", massivemilestone=count, count=count)
 
-    # Render HTML with count variable
+    if count % 100 == 0:
+        return render_template("index.html", hugemilestone=count, count=count)
+
+    if count % 50 == 0:
+        return render_template("index.html", bigmilestone=count, count=count)
+
+    if count % 25 == 0:
+        return render_template("index.html", milestone=count, count=count)
+    
     return render_template("index.html", count=count)
+
+
 
 if __name__ == "__main__":
     app.run()
